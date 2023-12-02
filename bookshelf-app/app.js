ed699a3983db9d561e765577bca91351b0daf422
Muhammad Saddam Pradana <neolinedev@gmail.com>
2023-12-02 20:42:46 +0700

    add search feature

--- bookshelf-app/app.js
+++ bookshelf-app/app.js
@@ -10,6 +10,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const year = document.getElementById("year").value;
     const isComplete = document.getElementById("isComplete").checked;
 
+    if (!title || !author || !year) {
+      alert("Seluruh Field Tidak Boleh Di Kosongkan !!!");
+      return;
+    }
+
     const newBook = {
       id: +new Date(),
       title,
@@ -108,6 +113,88 @@ document.addEventListener("DOMContentLoaded", () => {
     books = books.filter((book) => book.id !== bookId);
     localStorage.setItem("books", JSON.stringify(books));
     renderBookshelf();
+  };
+
+  const searchInput = document.getElementById("search-input");
+
+  searchInput.addEventListener("keyup", (event) => {
+    event.preventDefault();
+
+    if (event.keyCode === 13) {
+      searchBook(event.target.value);
+    }
+  });
+
+  const searchBook = (keyword) => {
+    const books = JSON.parse(localStorage.getItem("books")) || [];
+
+    const filteredBooks = books.filter((book) => {
+      return book.title.toLowerCase().includes(keyword.toLowerCase());
+    });
+
+    const completedBooksContainer = document.getElementById("completedBookList");
+    const uncompletedBooksContainer = document.getElementById("uncompletedBookList");
+
+    completedBooksContainer.innerHTML = "";
+    uncompletedBooksContainer.innerHTML = "";
+
+    let completedBooksCount = 0;
+    let uncompletedBooksCount = 0;
+
+    filteredBooks.forEach((book) => {
+      const bookItem = document.createElement("div");
+      bookItem.classList.add("book-item");
+      const status = book.isComplete ? "completed-books" : "uncompleted-books";
+
+      const html = `
+        <h3>${book.title}</h3>
+        <p>Penulis: ${book.author}</p>
+        <p>Tahun: ${book.year}</p>
+        <div class="action">
+          <button id="changeBookStatus" class="green">${book.isComplete ? "Belum selesai dibaca" : "Selesai dibaca"}</button>
+          <button id="deleteBook" class="red">Hapus buku</button>
+        </div>
+      `;
+
+      bookItem.innerHTML = html;
+
+      bookItem.querySelector("#changeBookStatus").addEventListener("click", () => {
+        changeBookStatus(book.id);
+      });
+
+      bookItem.querySelector("#deleteBook").addEventListener("click", () => {
+        alert(`Apakah anda yakin ingin menghapus buku ${book.title}?`);
+        deleteBook(book.id);
+      });
+
+      if (book.isComplete) {
+        completedBooksContainer.appendChild(bookItem);
+        completedBooksCount++;
+      } else {
+        uncompletedBooksContainer.appendChild(bookItem);
+        uncompletedBooksCount++;
+      }
+    });
+
+    document.querySelector(".completed-books .book-status").innerText = `Buku yang sudah selesai dibaca (${completedBooksCount})`;
+    document.querySelector(".uncompleted-books .book-status").innerText = `Buku yang belum selesai dibaca (${uncompletedBooksCount})`;
+
+    if (filteredBooks.length === 0) {
+      completedBooksContainer.innerHTML = `<p class="empty">Buku tidak ditemukan</p>`;
+      uncompletedBooksContainer.innerHTML = `<p class="empty">Buku tidak ditemukan</p>`;
+    }
+
+    searchInput.value = "";
+
+    const searchResult = document.getElementById("search-result");
+
+    searchResult.innerHTML = `
+      Hasil pencarian untuk: ${keyword}
+    `;
+
+    searchResult.style.display = "block";
+
+    return;
   };
 
   renderBookshelf();
